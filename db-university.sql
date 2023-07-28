620e05db42c319e2cd3c9eb9f0027a8b1215050c
Skyfe24 <fabio.erase@gmail.com>
2023-07-28 23:39:04 +0200

    fifth query

--- db-university.sql
+++ db-university.sql
@@ -16,3 +16,13 @@ SELECT * FROM `courses` WHERE cfu > 10;
 SELECT * FROM `students` WHERE year(date_of_birth)< 1992; 
 
 
+4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
+laurea (286)
+
+
+SELECT * FROM `courses`WHERE `period` LIKE 'I %' AND `year` = 1;
+
+5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+20/06/2020 (21)
+
+SELECT * FROM `exams` WHERE DATE(`date`) = '2020-06-20' AND TIME(`hour`) > '14:00';
