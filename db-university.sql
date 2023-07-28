ca4fc723b2495f9b2316bac7058dd56daa2865ab
Skyfe24 <fabio.erase@gmail.com>
2023-07-28 18:00:07 +0200

    third query

--- db-university.sql
+++ db-university.sql
@@ -10,6 +10,9 @@ SELECT * FROM `students` WHERE YEAR(date_of_birth) = '1990';
 SELECT * FROM `courses` WHERE cfu > 10;
 
 
+3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+
+SELECT * FROM `students` WHERE year(date_of_birth)< 1992; 
 
 
