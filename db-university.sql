f35c9fdd9016933c60c8f3ac6dcfc5cb4da424fb
Skyfe24 <fabio.erase@gmail.com>
2023-07-29 00:02:30 +0200

    FIRST QUERY from GROUP BY LIST

--- db-university.sql
+++ db-university.sql
@@ -39,3 +39,20 @@ SELECT * FROM `departments`;
 
 SELECT * FROM `teachers` WHERE `phone`is NULL;
 
+
+/*1. QUERY CON GROUP BY  */
+
+/*1. Contare quanti iscritti ci sono stati ogni anno  */
+
+SELECT COUNT(*) FROM `students` GROUP BY `enrolment_date`;
+
+/*2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio  */
+
+
+
+/*3. Calcolare la media dei voti di ogni appello d'esame  */
+
+
+
+/*4. Contare quanti corsi di laurea ci sono per ogni dipartiment  */
+
